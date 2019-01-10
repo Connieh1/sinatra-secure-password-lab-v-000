02ebc95e04b8389a30ed404415b4fe1a869ccf0b
Connie Hyman <constance.hyman@mchsi.com>
2019-01-10 03:49:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(username: params['username'], password: params['password'])
     if username == "" | password == ''
       redirect '/login'
+    end
 
   end
 
