466485947dcb8963d2d8b0fe329b36b3d8d0b8d7
Connie Hyman <constance.hyman@mchsi.com>
2019-01-10 04:04:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(username: params['username'], password: params['password'])
     if params[:username] = "" || params[:password] = ""
       redirect '/failure'
-    else User.create(username: params['username'], password: params['password'])
+    else User.create(username: params[:username], password: params[:password])
       redirect '/login'
     end
   end
